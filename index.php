628ec63de7717534c993403e8a01f1c0ef10cff8
Alexander Baranov <Caseynorth182@gmail.com>
2021-04-11 06:57:01 -0700

    add composer & autoloader psr-4 & .htaccess

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+require_once __DIR__. "/vendor/autoload.php";
+/*
+ *
+ *  login page - sign in (GET) - авторизация
+ *  / register - sign up (GET) - регистрация
+ *  / auth/login (POST) -> form
+ *  /dashboard - дорступна только админу
+ *  / profile - доступна админу и пользователю
+ *
+ * */
+
+
+var_dump($_GET['q']);
